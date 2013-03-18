236442e1e632730eec0008d531e9613f8cbbc2d0
Bobby Koller <kolbobby@gmail.com>
2013-03-18 01:06:33 -0400

    Working on template

--- application/views/template.php
+++ application/views/template.php
@@ -15,12 +15,20 @@
 	<body>
 		<div class="navbar navbar-static-top navbar-inverse">
 			<div class="navbar-inner">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+			
 				<a class="brand" href="/">cForum</a>
 				
-				<ul class="nav pull-right">
-					<li><a href="/">Home</a></li>
-					<li><a href="/kodi">For Kodi :)</a></li>
-				</ul>
+				<div class="nav-collapse collapse">
+					<ul class="nav pull-right">
+						<li><a href="/">Home</a></li>
+						<li><a href="/kodi">For Kodi :)</a></li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	
