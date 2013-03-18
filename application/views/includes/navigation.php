d5901d7ceec5eb61599a7f64b331080e3e64cb3f
Bobby Koller <kolbobby@gmail.com>
2013-03-18 01:15:39 -0400

    Working on navigation

---
+++ application/views/includes/navigation.php
@@ -0,0 +1,18 @@
+<div class="navbar navbar-static-top navbar-inverse">
+	<div class="navbar-inner">
+		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
+	
+		<a class="brand" href="/">cForum</a>
+		
+		<div class="nav-collapse collapse">
+			<ul class="nav pull-right">
+				<li><a href="/">Home</a></li>
+				<li><a href="/kodi">For Kodi :)</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
