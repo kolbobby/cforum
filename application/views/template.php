11ed5e1af04651c2fff3a8c11774fcf3058206cd
Bobby Koller <kolbobby@gmail.com>
2013-03-18 00:50:19 -0400

    Working on template

--- application/views/template.php
+++ application/views/template.php
@@ -3,7 +3,13 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?= $title; ?></title>
-		<link href="<?= base_url(); ?>assets/css/bootstrap.css" />
+		<link rel="StyleSheet" href="<?= base_url(); ?>assets/css/bootstrap.css" />
+		<style type="text/css">
+			.hero-unit {
+				padding-top:10px;
+				padding-bottom:5px;
+			}
+		</style>
 	</head>
 
 	<body>
