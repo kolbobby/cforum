613f37d57e842da975193350c9f32615c98865e9
Bobby Koller <kolbobby@gmail.com>
2013-03-18 01:15:22 -0400

    Working on template

--- application/views/template.php
+++ application/views/template.php
@@ -13,24 +13,7 @@
 	</head>
 
 	<body>
-		<div class="navbar navbar-static-top navbar-inverse">
-			<div class="navbar-inner">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-			
-				<a class="brand" href="/">cForum</a>
-				
-				<div class="nav-collapse collapse">
-					<ul class="nav pull-right">
-						<li><a href="/">Home</a></li>
-						<li><a href="/kodi">For Kodi :)</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<?php include_once "includes/navigation.php"; ?>
 	
 		<div class="hero-unit"> <?php $this->load->view($content); ?> </div>
 	</body>
