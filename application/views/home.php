47570585c0f1b8bd76b3ccafc201edcf540c9d79
Bobby Koller <kolbobby@gmail.com>
2013-03-18 21:42:03 -0400

    Adding database and JQuery

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,2 @@
 <h1>Welcome to cForum!</h1>
-<span class="draggable ui-widget-content">This is draggable!</span>
-
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('.draggable').draggable();
-	})
-</script>
+<div class="draggable ui-widget-content">This is draggable!</div>
